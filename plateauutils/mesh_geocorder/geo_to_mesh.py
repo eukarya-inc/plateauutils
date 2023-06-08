c02ea7c81e45e957d65e0c4c2827229dc806a1ca
Taro Matsuzawa <btm@tech.email.ne.jp>
2023-06-08 14:50:33 +0900

    WIP: implement meshcode to polygon function, add types

--- plateauutils/mesh_geocorder/geo_to_mesh.py
+++ plateauutils/mesh_geocorder/geo_to_mesh.py
@@ -1,22 +1,26 @@
 # coding: utf-8
 
 import math
-from shapely.geometry import Point
+from shapely.geometry import Point, Polygon
 
 
 class MeshException(Exception):
     pass
 
 
-def _validate_mesh(mesh: str):
-    """メッシュコードのバリデーション"""
+class MeshCodeException(Exception):
+    pass
+
+
+def _validate_mesh(mesh: str) -> None:
+    """メッシュのバリデーション"""
     if mesh not in ["1", "2", "2/1", "3", "4/1"]:
         raise MeshException("Mesh must be one of 1, 2, 2/1, 3, 4/1")
 
 
-def point_to_meshcode(point: Point, mesh: str = "2"):
+def point_to_meshcode(point: Point, mesh: str = "2") -> str:
     """緯度経度をメッシュコードに変換して返す"""
-    # メッシュコードのバリデーション
+    # メッシュのバリデーション
     _validate_mesh(mesh)
 
     # メッシュコードの初期化
@@ -55,7 +59,6 @@ def point_to_meshcode(point: Point, mesh: str = "2"):
         + math.floor(((longitude - math.floor(longitude)) * 60) % 7.5 * 60 % 45 / 22.5)
         + 1
     )
-    print(mesh_2nd1)
     mesh_code = mesh_code + f"{mesh_2nd1:01d}"
     if mesh == "2/1":
         return mesh_code
@@ -72,3 +75,61 @@ def point_to_meshcode(point: Point, mesh: str = "2"):
     mesh_code = mesh_code + f"{mesh_4th1}"
     if mesh == "4/1":
         return mesh_code
+
+
+def meshcode_to_polygon(mesh_code: str) -> Polygon:
+    """
+    メッシュコードをポリゴンに変換して返す
+    """
+    if len(mesh_code) < 4:
+        raise MeshCodeException("Mesh code must be 4 or more digits")
+
+    left_x = 0
+    right_x = 0
+    left_y = 0
+    right_y = 0
+
+    # 1次メッシュ
+    mesh_y_1st = int(mesh_code[:2])
+    mesh_x_1st = int(mesh_code[2:4])
+    left_x = float(mesh_x_1st + 100)
+    left_y = float(mesh_y_1st * 40 / 60)
+
+    # 1次メッシュの場合
+    if len(mesh_code) == 4:
+        right_x = left_x + 1
+        right_y = left_y + 1 * 40 / 60
+        return _create_polygon(left_x, left_y, right_x, right_y)
+
+    # 2次メッシュ
+    mesh_y_2nd = int(mesh_code[4:5])
+    mesh_x_2nd = int(mesh_code[5:6])
+    left_x = left_x + mesh_x_2nd * 7.5 / 60
+    left_y = left_y + mesh_y_2nd * 5 / 60
+
+    # 2次メッシュの場合
+    if len(mesh_code) == 6:
+        right_x = left_x + (1 * 7.5 / 60)
+        right_y = left_y + (1 * 5 / 60)
+        return _create_polygon(left_x, left_y, right_x, right_y)
+
+    # 3次メッシュ
+    mesh_y_3rd = int(mesh_code[6:7])
+    mesh_x_3rd = int(mesh_code[7:8])
+    left_x = left_x + mesh_x_3rd * 45 / 60 / 60
+
+
+def _create_polygon(
+    left_x: float, left_y: float, right_x: float, right_y: float
+) -> Polygon:
+    """
+    バウンディングボックスからポリゴンを作成して返す
+    """
+    coords = (
+        (left_x, left_y),
+        (right_x, left_y),
+        (right_x, right_y),
+        (left_x, right_y),
+        (left_x, left_y),
+    )
+    return Polygon(coords)
--- plateauutils/mesh_geocorder/tests/test_geo_to_mesh.py
+++ plateauutils/mesh_geocorder/tests/test_geo_to_mesh.py
@@ -32,3 +32,34 @@ def test_point_to_mesh():
 
     mesh_code = point_to_meshcode(point, "4/1")
     assert mesh_code == "5339454711"
+
+
+def test_meshcode_to_polygon():
+    """メッシュコードからポリゴンを生成するテスト"""
+    from plateauutils.mesh_geocorder.geo_to_mesh import meshcode_to_polygon
+
+    mesh_code = "5339"
+    polygon = meshcode_to_polygon(mesh_code)
+    assert (
+        polygon.wkt
+        == "POLYGON ((139 35.333333333333336, 140 35.333333333333336, 140 36, 139 36, 139 35.333333333333336))"
+    )
+
+    mesh_code = "533900"
+    polygon = meshcode_to_polygon(mesh_code)
+    assert (
+        polygon.wkt
+        == "POLYGON ((139 35.333333333333336, 139.125 35.333333333333336, 139.125 35.41666666666667, 139 35.41666666666667, 139 35.333333333333336))"
+    )
+
+    mesh_code = "533945"
+    polygon = meshcode_to_polygon(mesh_code)
+    assert (
+        polygon.wkt
+        == "POLYGON ((139.625 35.66666666666667, 139.75 35.66666666666667, 139.75 35.75000000000001, 139.625 35.75000000000001, 139.625 35.66666666666667))"
+    )
+
+    # TODO: implement
+    mesh_code = "53394500"
+    polygon = meshcode_to_polygon(mesh_code)
+    assert polygon == None
